3a289a0a286216bc7a560b021a822aaad3c2c635
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-05 13:04:52 +0900

    added some more notes

--- api/views.py
+++ api/views.py
@@ -5,9 +5,13 @@ from .models import Bucketlist
 # Create your views here.
 # ListCreateAPIView is generic view which provides GET (list all) / POST method
 # handlers
+# APIView is the base class, takes care of all the routing for you
 class CreateView(generics.ListCreateAPIView):
     """defines create behavior of rest api"""
     # query set is the collection of items we are trying to view
+    # ListCreateAPIView takes of GET and POST for you, you use this because
+    # this endpoint only supports a list of objects
+    # DRF responses are completely unrendered, they return DATA and status_code
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
 
